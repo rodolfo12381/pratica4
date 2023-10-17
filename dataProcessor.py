800a54880f0335f0e7f221e74903bc63f608021d
Rodolfo Rocha Rodrigues <rodolfo.rocha@HINOVA.net>
2023-10-17 10:40:07 -0300

    adicionando arquivos necessarios

---
+++ dataProcessor.py
@@ -0,0 +1,12 @@
+import json
+
+def read_json_file(file_path):
+
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File not found: {file_path}")
+    except json.JSONDecodeError:
+        raise ValueError(f"Invalid JSON format in file: {file_path}")
