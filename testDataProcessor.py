5072113e3f0018ec41d1bcfe256c84f3b8223341
Rodolfo Rocha Rodrigues <rodolfo.rocha@HINOVA.net>
2023-10-22 22:31:09 -0300

    adicionando scripts necessários

---
+++ testDataProcessor.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+from dataProcessor import read_json_file
+
+class TestDataProcessor(unittest.TestCase):
+    def test_read_json_file_success(self):
+        current_directory = os.path.dirname(__file__)
+        file_path = os.path.join(current_directory, "users.json")
+
+        data = read_json_file(file_path)
+       
+        self.assertEqual(len(data), 1000)  # Ajustar o número esperado de registros
+        self.assertEqual(data[0]['name'], 'Alfred Clark')
+        self.assertEqual(data[1]['age'], 18)
+
+    def test_read_json_file_file_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            read_json_file("non_existent.json")
+
+    def test_read_json_file_invalid_json(self):
+        with open("invalid.json", "w") as file:
+            file.write("invalid json data")
+        with self.assertRaises(ValueError):
+            read_json_file("invalid.json")
+
+if __name__ == '__main__':
+    unittest.main()
