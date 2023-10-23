56c1d8f640becc1d893aa96ea84cad428b73cd00
Rodolfo Rocha Rodrigues <rodolfo.rocha@HINOVA.net>
2023-10-23 08:15:34 -0300

    adicionando casos de teste novos

--- testDataProcessor.py
+++ testDataProcessor.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 from dataProcessor import read_json_file
+from dataProcessor import avgAgeCountry, transform_age
 
 class TestDataProcessor(unittest.TestCase):
     def test_read_json_file_success(self):
@@ -22,6 +23,39 @@ class TestDataProcessor(unittest.TestCase):
             file.write("invalid json data")
         with self.assertRaises(ValueError):
             read_json_file("invalid.json")
+
+    def test_avgAgeCountry_empty_json(self):
+        current_directory = os.path.dirname(__file__)
+        file_path = os.path.join(current_directory, "users.json")
+
+        data = read_json_file(file_path)
+        result = avgAgeCountry(data)
+        self.assertEqual(result, {})
+
+    def test_avgAgeCountry_missing_age_values(self):
+        current_directory = os.path.dirname(__file__)
+        file_path = os.path.join(current_directory, "users.json")
+
+        data = read_json_file(file_path)
+        result = avgAgeCountry(data)
+        self.assertEqual(result, {"USA": None, "Canada": None})
+
+    def test_avgAgeCountry_missing_country_values(self):
+        current_directory = os.path.dirname(__file__)
+        file_path = os.path.join(current_directory, "users.json")
+
+        data = read_json_file(file_path)
+        result = avgAgeCountry(data)
+        self.assertEqual(result, {"Unknown": 27.5})
+
+    def test_avgAgeCountry_with_age_transformation(self):
+        current_directory = os.path.dirname(__file__)
+        file_path = os.path.join(current_directory, "users.json")
+
+        data = read_json_file(file_path)
+        result = avgAgeCountry(data, transform_age)
+        self.assertEqual(result, {"USA": 300, "Canada": 360})
+
 
 if __name__ == '__main__':
     unittest.main()
